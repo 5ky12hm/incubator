83390332b7cfafc82b5ddac4f6b35444e330f408
5ky12hm <190591493+5ky12hm@users.noreply.github.com>
2024-12-07 13:44:48 +0900

    feat(usefools): create clipboard translation tool

---
+++ usefools/mac/transclip.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import pyperclip
+from googletrans import Translator
+from AppKit import NSWorkspace
+
+
+class Transclip:
+    allow_appnames = ["wezterm"]
+    srcLang = "en"
+    destLang = "ja"
+
+    def run(self):
+        translator = Translator()
+        workspace = NSWorkspace.sharedWorkspace()
+        log, sentence = None, None
+
+        while True:
+            while sentence is None or sentence == log:
+                time.sleep(0.1)
+
+                sentence = pyperclip.paste()
+                if sentence is None:
+                    continue
+
+                if log is None:
+                    log = sentence
+                    continue
+
+            log = sentence
+
+            appname = workspace.activeApplication()["NSApplicationName"]
+            if appname not in self.allow_appnames:
+                print(appname)
+                continue
+
+            detected = translator.detect(sentence)
+            if detected != self.srcLang:
+                continue
+
+            translated = translator.translate(
+                sentence, src=self.srcLang, dest=self.destLang).text
+
+            os.system(f"osascript -e 'display dialog \"{translated}\"'")
+
+
+if __name__ == "__main__":
+    Transclip().run()
